27cbc454bc52d112d56df0e024069607307aa2d9
YLF-Cat <1956702442@qq.com>
2025-05-07 21:58:48 +0800

    Site updated: 2025-05-07 21:58:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/04/hello-world/"];function toRandomPost(){
+var posts=["2025/05/04/welcome/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

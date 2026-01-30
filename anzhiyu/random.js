a1969baa2d4dd90e02484cc9351b40f93e65fdf9
YLF-Cat <1956702442@qq.com>
2026-01-30 23:22:15 +0800

    Site updated: 2026-01-30 23:22:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/15/cspslist/","2026/01/25/daily_25_1/","2026/01/24/daily_prologue/","2025/05/04/welcome/","2025/08/03/suyi1st/","2025/06/03/ic_test/"];function toRandomPost(){
+var posts=["2025/10/15/cspslist/","2025/05/04/welcome/","2025/06/03/ic_test/","2025/08/03/suyi1st/","2026/01/24/daily_prologue/","2026/01/25/daily_25_1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

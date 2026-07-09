edc0349972bed9438c2bfa0753b38716a3a230b0
YLF-Cat <1956702442@qq.com>
2026-07-10 00:40:05 +0800

    Site updated: 2026-07-10 00:40:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["cspslist/","OI2607/","suyi1st/","ic_test/","icvibelua/","welcome/"];function toRandomPost(){
+var posts=["cspslist/","OI2607/","ic_test/","suyi1st/","icvibelua/","welcome/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

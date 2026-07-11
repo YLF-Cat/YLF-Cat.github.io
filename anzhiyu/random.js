4378e6a05a0eb1e3a49178363a13045a36dae7ba
YLF-Cat <1956702442@qq.com>
2026-07-11 13:57:39 +0800

    Site updated: 2026-07-11 13:57:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["cspslist/","OI2607/","ic_test/","suyi1st/","icvibelua/","welcome/"];function toRandomPost(){
+var posts=["OI2607/","suyi1st/","ic_test/","cspslist/","welcome/","icvibelua/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
